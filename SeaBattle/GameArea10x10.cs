321004461ae45a6848bd06396fe492f6355e8484
Vlad <vladrobilko@gmail.com>
2022-08-08 13:33:22 +0300

    переделка

--- SeaBattle/GameArea10x10.cs
+++ SeaBattle/GameArea10x10.cs
@@ -1,49 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeaBattle
 {
 
     public class GameArea10x10
     {
-        int[][] sqare10x10;
-
         static Random rnd = new Random();
-        public GameArea10x10()
+
+
+        public int[][] sqare10x10;//это и метод ниже свойствами сделать, или хз
+        public void Area10x10()
         {
-            int[][] arrrey = new int[10][];
+            int[][] array = new int[10][];
             for (int i = 0; i < 10; i++)
             {
-                arrrey[i] = new int[10];
+                array[i] = new int[10];
             }
-            this.sqare10x10 = arrrey;
+            this.sqare10x10 = array;
         }
 
-        public void FillGameArea()
+        public int[][] FillGameArea()
         {
-            int a = rnd.Next(10);
-            int b = rnd.Next(10);
-            this.sqare10x10[a][b] = 1;
+            CreateShipLenght4(rnd.Next(9), rnd.Next(9));
+
+            return this.sqare10x10;
+        }
+        public void CreateShipLenght4(int x, int y)
+        {
             try
             {
-
+                Area10x10();
+                int a = x;
+                int b = y;
+                this.sqare10x10[a][b] = 1;
+                for (int i = 0; i < 3; i++)
+                {
+                    ++a;
+                    if (a>9||b>9)
+                    {
+                        break;
+                    }
+                    this.sqare10x10[a][b] = 1;
+                }//mnmnm
             }
             catch (Exception)
             {
+                try
+                {
+                    Area10x10();
+                    int c = x;
+                    int d = y;
+                    this.sqare10x10[c][d] = 1;
+                    for (int i = 0; i < 3; i++) this.sqare10x10[c][--d] = 1;
 
-                throw;
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        Area10x10();
+                        int f = x;
+                        int g = y;
+                        this.sqare10x10[f][g] = 1;
+                        for (int i = 0; i < 3; i++) this.sqare10x10[f][++g] = 1;
+
+                    }
+                    catch (Exception)
+                    {
+                        Area10x10();
+                        this.sqare10x10[x][y] = 1;
+                        for (int i = 0; i < 3; i++) this.sqare10x10[--x][y] = 1;
+
+                    }
+                }
+
+
             }
 
         }
-
-        public void CreatShip(int a)
-        {
-
-        }
-
 
     }
 }
