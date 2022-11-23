2da66649e4774794dc21bdec86c19f798bf024de
vladrobilko <vladrobilko@gmail.com>
2022-11-23 10:26:12 +0400

    naming

--- WebApiSeaBattleClient/WebApiSeaBattleClient/Program.cs
+++ WebApiSeaBattleClient/WebApiSeaBattleClient/Program.cs
@@ -1,8 +1,5 @@
-﻿using System.IO;
+﻿using SeaBattle.ApiClientModels;
 using System.Net.Http.Json;
-using System.Xml.Linq;
-using SeaBattleApi.Models;
-using SeaBattleApi.Models.Interfaces;
 
 class Program
 {
@@ -12,32 +9,32 @@ class Program
         while (key != ConsoleKey.Escape)
         {
             Information();
-            key = Console.ReadKey().Key;                        
+            key = Console.ReadKey().Key;
             if (key == ConsoleKey.Enter)
             {
                 await CreatePlayer("https://localhost:7109/api/PlayerClient/Login", "Vasya");//register 
             }
             else if (key == ConsoleKey.F1)
             {
-                await GetPlayerByName("https://localhost:7109/api/PlayerClient/GetByName","Vasya");
+                //await GetPlayerByName("https://localhost:7109/api/PlayerClient/GetByName", "Vasya");
             }
 
             else if (key == ConsoleKey.F2)
             {
-                CreateSession("https://localhost:7109/api/SeaBattleSession/Create", _player, "Sea Battle");
+                //CreateSession("https://localhost:7109/api/SeaBattleSession/Create", _player, "Sea Battle");
             }
 
             else if (key == ConsoleKey.F3)
             {
-                GetAllSession("https://localhost:7109/api/SeaBattleSession/GetAll", _seaBattleGameSession.ID);
+                //GetAllSession("https://localhost:7109/api/SeaBattleSession/GetAll", _seaBattleGameSession.ID);
             }
         }
     }
     private static readonly HttpClient client = new HttpClient();
 
-    private static PlayerClient _player;
+    private static PlayerClientModel _player;
 
-    private static SeaBattleGameSession _seaBattleGameSession;
+    //private static Session _seaBattleGameSession;
 
     private static async Task CreatePlayer(string path, string playerName)
     {
@@ -45,26 +42,26 @@ class Program
         Console.WriteLine($"Status code: {response.StatusCode}");
     }
 
-    private static async Task GetPlayerByName(string path, string name)
-    {
-        using HttpResponseMessage response = await client.PostAsJsonAsync(path, name);
-        _player = await response.Content.ReadFromJsonAsync<PlayerClient>();
-        if (_player != null)
-        {
-            Console.WriteLine($"Name: {_player.Name}, ID: {_player.ID}, time adding: {_player.TimeAdding}");
-            return;
-        }
-        Console.WriteLine($"The player isn't created. Error: {response.StatusCode}");
-    }
+    //private static async Task GetPlayerByName(string path, string name)
+    //{
+    //    using HttpResponseMessage response = await client.PostAsJsonAsync(path, name);
+    //    _player = await response.Content.ReadFromJsonAsync<PlayerClient>();
+    //    if (_player != null)
+    //    {
+    //        Console.WriteLine($"Name: {_player.Name}, ID: {_player.ID}, time adding: {_player.TimeAdding}");
+    //        return;
+    //    }
+    //    Console.WriteLine($"The player isn't created. Error: {response.StatusCode}");
+    //}
 
-    private static async Task CreateSession(string path, PlayerClient playerClient, string nameGame)
-    {
-        _seaBattleGameSession = new SeaBattleGameSession();
-        _seaBattleGameSession.PlayerHost = playerClient;
-        _seaBattleGameSession.Name = nameGame;
-        using HttpResponseMessage response = await client.PostAsJsonAsync(path, _seaBattleGameSession);
-        Console.WriteLine($"Status code: {response.StatusCode}");
-    }
+    //private static async Task CreateSession(string path, PlayerClient playerClient, string nameGame)
+    //{
+    //    _seaBattleGameSession = new SeaBattleGameSession();
+    //    _seaBattleGameSession.PlayerHost = playerClient;
+    //    _seaBattleGameSession.Name = nameGame;
+    //    using HttpResponseMessage response = await client.PostAsJsonAsync(path, _seaBattleGameSession);
+    //    Console.WriteLine($"Status code: {response.StatusCode}");
+    //}
 
     private static async Task GetAllSession(string path, string idSession)
     {
